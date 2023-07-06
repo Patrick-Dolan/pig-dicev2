0b1e109adec50510d923c3cb749ecdf8b73c462b
Patrick Dolan <dolanp1992@gmail.com>
2023-07-06 11:49:47 -0700

    add game information display and play buttons

--- js/scripts.js
+++ js/scripts.js
@@ -72,17 +72,40 @@ Game.prototype.getWinner = function() {
 // UI Logic
 // ==========================
 
+function displayCurrentRoundInfo(game) {
+  document.querySelector("#current-player-turn-display").innerText = game.players[game.playerTurn].name;
+  document.querySelector("#round-score-display").innerText = game.roundScore;
+  // TODO replace following score displays with dynamically changing values for player
+  document.querySelector("#player-one-score-display").innerText = game.players[0].score;
+  document.querySelector("#player-two-score-display").innerText = game.players[1].score;
+}
+
 window.addEventListener("load", () => {
   let game = new Game();
 
   document.getElementById("player-name-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    document.querySelector("#form-display").classList.add("hidden");
     let playerOneName = document.querySelector("#playerOneName").value;
     let playerTwoName = document.querySelector("#playerTwoName").value;
     let playerOne = new Player(playerOneName);
     let playerTwo = new Player(playerTwoName);
     game.addPlayer(playerOne);
     game.addPlayer(playerTwo);
+    
+    document.querySelector("#player-one-name-display").innerText = playerOne.name; 
+    document.querySelector("#player-two-name-display").innerText = playerTwo.name; 
+    document.querySelector("#form-display").classList.add("hidden");
+    document.querySelector("#game-display").classList.add("show");
+    displayCurrentRoundInfo(game)
+  });
+
+  document.getElementById("roll-button").addEventListener("click", () => {
+    let currentRoll = game.rollDice();
+    game.evaluateRoll(currentRoll);
+    displayCurrentRoundInfo(game);
+  });
+
+  document.getElementById("hold-button").addEventListener("click", () => {
+    alert("hold clicked");
   });
 });
