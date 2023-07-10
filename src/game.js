0a1dd6e62b41d4b61713087cedfbede58fd1f0cd
Patrick Dolan <dolanp1992@gmail.com>
2023-07-10 14:40:42 -0700

    refactor game to es6 class

--- src/game.js
+++ src/game.js
@@ -1,50 +1,52 @@
-export default function Game() {
-  this.players = [];
-  this.playerTurn = 0;
-  this.roundScore = 0;
-  this.gameOver = false;
-}
-
-Game.prototype.addPlayer = function(player) {
-  this.players.push(player);
-};
-
-Game.prototype.rollDice = function() {
-  return Math.floor(Math.random() * 6) + 1;
-};
-
-Game.prototype.changeTurns = function() {
-  if (this.playerTurn === 0) {
-    this.playerTurn = 1;
-  } else {
+export default class Game {
+  constructor() {
+    this.players = [];
     this.playerTurn = 0;
+    this.roundScore = 0;
+    this.gameOver = false;
   }
-};
 
-Game.prototype.evaluateRoll = function(currentRoll) {
-  if (currentRoll === 1) {
+  addPlayer(player) {
+    this.players.push(player);
+  }
+
+  rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  changeTurns() {
+    if (this.playerTurn === 0) {
+      this.playerTurn = 1;
+    } else {
+      this.playerTurn = 0;
+    }
+  }
+
+  evaluateRoll(currentRoll) {
+    if (currentRoll === 1) {
+      this.roundScore = 0;
+      this.changeTurns();
+    } else {
+      this.roundScore += currentRoll;
+    }
+  }
+
+  holdAction() {
+    this.players[this.playerTurn].addScore(this.roundScore);
+    if (this.players[this.playerTurn].isWinner()) {
+      this.gameOver = true;
+    }
     this.roundScore = 0;
     this.changeTurns();
-  } else {
-    this.roundScore += currentRoll;
   }
-};
 
-Game.prototype.holdAction = function() {
-  this.players[this.playerTurn].addScore(this.roundScore);
-  if (this.players[this.playerTurn].isWinner()) {
-    this.gameOver = true;
+  getWinner() {
+    let winner = { score: 0 };
+    this.players.forEach((player) => {
+      if (player.score > winner.score) {
+        winner = player;
+      }
+    });
+    return winner;
   }
-  this.roundScore = 0;
-  this.changeTurns();
-};
-
-Game.prototype.getWinner = function() {
-  let winner = {score: 0};
-  this.players.forEach((player) => {
-    if (player.score > winner.score) {
-      winner = player;
-    }
-  });
-  return winner;
-};
+}
