17b81c60485d34a25d9875bbe2194a9611a82751
Patrick Dolan <dolanp1992@gmail.com>
2023-07-10 11:24:49 -0700

    add Game addPlayer method tests

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,10 +1,15 @@
 import Game from "../src/game";
+import Player from "../src/player";
 
 describe("Game", () => {
   let game;
+  let playerOne;
+  let playerTwo;
 
   beforeEach(() => {
     game = new Game();
+    playerOne = new Player("Patrick");
+    playerTwo = new Player("Abi");
   });
 
   test("should correctly create a game object that includes players array, playerTurn, roundScore, and gameOver prop", () => {
@@ -12,5 +17,19 @@ describe("Game", () => {
     expect(game.playerTurn).toEqual(0);
     expect(game.roundScore).toEqual(0);
     expect(game.gameOver).toBeFalsy();
+  });
+
+  describe("Game.prototype.addPlayer", () => {
+    test("should add a new player to the game objects players array", () => {
+      game.addPlayer(playerOne);
+      expect(game.players[0]).toEqual(playerOne);
+    });
+
+    test("should add two players to the game objects players array", () => {
+      game.addPlayer(playerOne);
+      game.addPlayer(playerTwo);
+      expect(game.players[0]).toEqual(playerOne);
+      expect(game.players[1]).toEqual(playerTwo);
+    });
   });
 });
